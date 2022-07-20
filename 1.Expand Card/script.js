227efc89d88ed9ca5615cb7bea74dfbd705e01e7
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2022-07-21 00:04:03 +0200

    Add files via upload

---
+++ 1.Expand Card/script.js
@@ -0,0 +1,17 @@
+/*
+
+const containerImgs = document.querySelectorAll(".container");
+
+containerImgs.forEach((container) => {
+  container.addEventListener("mouseover", function () {
+    removeActiveClass();
+    container.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  containerImgs.forEach((container) => {
+    container.classList.remove("active");
+  });
+}
+*/
